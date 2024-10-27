9cfb78963448c870e47c113a620da2ed0a2d1260
joshpg190498 <joseperez1910498@gmail.com>
2024-10-27 03:00:22 -0500

    refactor code

--- modules/data-acquisition/config/config.go
+++ modules/data-acquisition/config/config.go
@@ -3,39 +3,24 @@ package config
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"ceiot-tf-sbc/modules/data-acquisition/models"
-
-	"github.com/joho/godotenv"
 )
 
 func LoadEnvVars() (*models.Config, error) {
-	dir, err := os.Getwd()
-	if err != nil {
-		return nil, fmt.Errorf("error al obtener el directorio actual: %w", err)
-	}
-
-	envFile := filepath.Join(dir, ".env")
-
-	err = godotenv.Load(envFile)
-	if err != nil {
-		return nil, fmt.Errorf("error loading .env file: %w", err)
-	}
-
 	idDevice := os.Getenv("ID_DEVICE")
 	mqttProtocol := os.Getenv("MQTT_PROTOCOL")
 	mqttHost := os.Getenv("MQTT_HOST")
 	mqttPort := os.Getenv("MQTT_PORT")
 
-	mqttClientID := fmt.Sprintf("mqtt-sbc-data-acquisition-%s", idDevice)
+	mqttClientID := fmt.Sprintf("background-data-acquisition-%s-mqtt-client", idDevice)
 	mqttBroker := fmt.Sprintf("%s://%s:%s", mqttProtocol, mqttHost, mqttPort)
 
-	mqttSubConfigTopic := fmt.Sprintf("SERVER/CONFIG/%s", idDevice)
+	mqttSubConfigTopic := fmt.Sprintf("server/config/%s", idDevice)
 	mqttSubTopics := []string{mqttSubConfigTopic}
 
-	mqttPubConfigTopic := fmt.Sprintf("DEVICES/%s/CONFIG", idDevice)
-	mqttPubDataTopic := fmt.Sprintf("DEVICES/%s/DATA", idDevice)
+	mqttPubConfigTopic := fmt.Sprintf("devices/%s/config", idDevice)
+	mqttPubDataTopic := fmt.Sprintf("devices/%s/data", idDevice)
 	databasePath := os.Getenv("SQLITE_DB_PATH")
 
 	config := &models.Config{
