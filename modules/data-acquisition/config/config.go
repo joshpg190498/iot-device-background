3d00335c3fa66d8369d7b31100bb8a99e30fa92d
joshpg190498 <joseperez1910498@gmail.com>
2024-10-30 04:18:43 -0500

    change in background

--- modules/data-acquisition/config/config.go
+++ modules/data-acquisition/config/config.go
@@ -3,11 +3,24 @@ package config
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"ceiot-tf-sbc/modules/data-acquisition/models"
+
+	"github.com/joho/godotenv"
 )
 
 func LoadEnvVars() (*models.Config, error) {
+	dir, err := os.Executable()
+	if err != nil {
+		return nil, fmt.Errorf("error al obtener el directorio actual: %w", err)
+	}
+	envFile := filepath.Join(filepath.Dir(dir), ".env")
+	err = godotenv.Load(envFile)
+	if err != nil {
+		return nil, fmt.Errorf("error loading .env file: %w", err)
+	}
+
 	idDevice := os.Getenv("ID_DEVICE")
 	mqttProtocol := os.Getenv("MQTT_PROTOCOL")
 	mqttHost := os.Getenv("MQTT_HOST")
